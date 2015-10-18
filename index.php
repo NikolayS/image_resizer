fe2026289f109a3f906fb4a22bbc7abf99fdab66
NikolayS <nikolay@startupturbo.com>
2015-10-18 19:41:14 +0300

    polishing

--- index.php
+++ index.php
@@ -1,25 +1,30 @@
 <?php
-//define("DEBUG", 1);
+define("DEBUG", 1);
+
+if (!extension_loaded('gd') && !extension_loaded('gd2')) {
+    trigger_error("GD is not loaded", E_USER_WARNING);
+    return false;
+}
 
 set_error_handler(function ($severity, $message, $filepath, $line) {
     throw new Exception($message . " in $filepath, line $line");
 }, E_ALL & ~E_STRICT & ~E_NOTICE);
 
 $SUPPORTED_TYPES = array(
-    'png' => 'image/png',
-    'jpg' => 'image/jpeg',
-    'gif' => 'image/gif',
+    'image/png',
+    'image/jpg',
+    'image/jpeg',
+    'image/gif',
 );
 
 try {
     $src = @$_GET['src'];
     if (!$src) {
-        throw new Exception("Required parameter not set: src.");
+        throw new Exception("Required parameter is not set: 'src'.");
     }
-    $src = urldecode($src);
     $data = file_get_contents($src);
     $headers = parseHeaders($http_response_header);
-    //print_r($headers);die;
+    print_r($headers);die;
 
     if (!isset($headers['content-type']) || !in_array(strtolower($headers['content-type']), $SUPPORTED_TYPES)) {
         throw new Exception("Either the file is not an image or its type is not supported.");
@@ -32,7 +37,7 @@ try {
     if (isset($_GET['w']) && ($resW = intval($_GET['w'])) && $resW > 0) { // resize, target: WIDTH
         $resH = round($resW * $h / $w);
         $resImg = imagecreatetruecolor($resW, $resH);
-        if (strtolower($headers['content-type']) == 'image/png') {
+        if (in_array(strtolower($headers['content-type']), array('image/png', 'image/gif'))) { // tricks to preserve PNG's transparency
             imagealphablending($resImg, false);
             imagesavealpha($resImg, true);
             $transparent = imagecolorallocatealpha($resImg, 255, 255, 255, 127);
@@ -42,7 +47,7 @@ try {
     } else {
         $resImg = $img;
     }
-    
+
     if (@$headers['content-length'] > 0 && strpos(@$headers['content-type'], "Image") == 0) {
         switch (strtolower($headers['content-type'])) {
         case 'image/jpeg':
@@ -52,7 +57,9 @@ try {
         case 'image/png':
             header("Content-Type: image/png");
             imagepng($resImg, NULL, 9);
-        //case 'image/gif':
+        case 'image/gif':
+            header("Content-Type: image/gif");
+            imagegif($resImg, NULL);
         default:
             $err = "Output for Content-Type='{$headers['content-type']}' is not yet implemented";
             header("X-IMAGE-RESIZER-ERROR: $err");
@@ -63,7 +70,7 @@ try {
     }
 } catch (Exception $e) {
     header("Bad request", true, 400);
-    header("X-IMAGE-RESIZER-ERROR: " . $e->getMessage());
+    header("X-IMAGE-RESIZER-ERROR: " . str_replace(array("\n", "\r"), array(" ", " "), $e->getMessage()));
     if (defined("DEBUG")) echo $e->getMessage();
     exit;
 }
