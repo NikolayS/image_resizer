da55007ad819e70fdfa65b618df817795dccbbcd
NikolayS <nikolay@startupturbo.com>
2016-02-29 22:13:23 +0300

    fixed test case for animated GID (result size changed after ImageMagick upgrade)

--- test/cases/animated_gif.sh
+++ test/cases/animated_gif.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # to run this scenario, RESIZERSERVICE should be defined (`export RESIZERSERVICE=https://your.resizer.host.name`)
+# NOTICE: if this test fails only due to slightly different size of result, it's OK – the size depents on yout ImageMagick version
 
 http GET "$RESIZERSERVICE/?w=200&src=https://upload.wikimedia.org/wikipedia/commons/2/2c/Rotating_earth_(large).gif" -h | grep -v "Date:"
